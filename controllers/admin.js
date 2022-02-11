0e048dd062dde8b22bbdc4dc6d7605004b13b173
NeDinked <mentesnot.ertibu@gmail.com>
2022-02-11 17:27:15 +0300

    modified admin report

--- controllers/admin.js
+++ controllers/admin.js
@@ -113,3 +113,53 @@ function decrypt(text) {
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
 }
+exports.getReport = async(req, res, next) => {
+  const [request, metaData] =await Request.fetchType();
+  let values=[];
+  for (let req of request){
+    values.push(req.val); 
+  }
+  //  console.log(request);
+  //  console.log(values);
+  res.render('admin/report', {req: request, type: values});
+}
+exports.getEmail = async (req, res, next) => {
+  let email = req.body.email_adress;
+  const id = req.body.id;
+  const message = req.body.response;
+  console.log(email);
+  email = 'hundaolnk@gmail.com';
+  Request.changeStatus(message, id);
+  
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'agnes.lockman80@ethereal.email', // generated ethereal user
+      pass: 'XRz9BuZ9eXTW4FMDCR', // generated ethereal password
+    },
+  });
+
+  const msg = {
+    from: '"The express app" <foo@example.com>', //  sender address
+    to: email, // list of receivers
+    subject: "sup", // Subject line
+    text: req.body.response, // plain text body
+  }
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(msg);
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  const request =await Request.fetchNew();
+  res.render('admin/request', {req: request[0]});
+}
